55e3de039ba48f439f0126814ea89a5db8ebbc5c
LisarWang <LisarWang@aliyun.com>
2016-11-26 16:16:25 +0800

    zhujie

---
+++ c1-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int result(int,int);
+const int k=2;
+strust Point{
+	int x,y;
+};
+
+int main()
+{
+	int z(0),(50);
+    Point a;
+    cout<<"输入两个整数（以空格区分）：“；
+    cin>>a.x>>a.y
+    z=(a.x+a.y)*k;
+	z=result(z,b);
+	cout<<"计算机结果如下：”<<endl;
+	cout<<"(("<<a.x<<"+"<<a.y
+	<<")*"<<k<<")+"<<b
+	<<"="<<z
+	<<endl:
+	return 0;
+}
+//***************************
+//*函 数：result            *
+//*参 数：整形对象a和b      *
+//*返回值：整形对象         ×
+//×××××××××××××××××××××××××××
+int result(int a,int b)
+{
+	rehuen a+b;
+}
